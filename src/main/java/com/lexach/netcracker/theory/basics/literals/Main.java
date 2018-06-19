f16a30c7dcf855c86192a423b9c7dc6941638b94
Alex Chernevskiy <lexachsar@gmail.com>
2018-06-19 19:43:28 +0400

    Added literals.

---
+++ src/main/java/com/lexach/netcracker/theory/basics/literals/Main.java
@@ -0,0 +1,38 @@
+package com.lexach.netcracker.theory.basics.literals;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        // Escape sequences
+
+        // \b backspace
+        System.out.println("AAAAAAAAAB\b");
+
+        // \f next page
+        System.out.println("First page\f");
+        System.out.println("Second page");
+
+        // \n next line
+        System.out.println("First line\n");
+        System.out.println("Second line");
+
+        // \r carriage return (start of the line)
+        System.out.println("AAAA\rBBBB");
+
+        // \t tabulation
+        System.out.println("Tab\tTab");
+
+        // \\ backslash
+        System.out.println("\\");
+
+        // \’ apostrophe
+        System.out.println("\'");
+
+        // \" quote
+        System.out.println("\"");
+
+    }
+
+}
